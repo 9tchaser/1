6f209f2a48c37b3de5f314c18db36b1ae8148eed
9tchaser <9tchaser@gmail.com>
2024-03-02 21:20:26 +0800

    2024/3/2

--- Code_Frame.py
+++ Code_Frame.py
@@ -1,17 +1,65 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset, DataLoader
+from torchvision.utils import save_image
 import numpy as np
+import pandas as pd
+import ast
+import cv2
 
-# 定义生成器模型
+#读取图像数据
+#define .xlsx filename and folder containing images and masks
+info_filename = r'D:\MONAI\CGAN_Image_sup\CGAN_Image\data\BrEaST-Lesions-USG-clinical-data-Dec-15-2023.xlsx'
+images_and_masks_foldername = r'D:\MONAI\CGAN_Image_sup\CGAN_Image\data\BrEaST-Lesions_USG-images_and_masks-Dec-15-2023/'
+
+#read .xlsx file with clinical data
+breast_dataset = pd.read_excel(info_filename, sheet_name='BrEaST-Lesions-USG clinical dat')
+
+for i in breast_dataset.index:
+    #parse milti-label columns
+    breast_dataset.at[i, 'Tissue_composition'] = breast_dataset.loc[i,'Tissue_composition'].split('&')
+    breast_dataset.at[i, 'Signs'] = breast_dataset.loc[i,'Signs'].split('&')
+    breast_dataset.at[i, 'Symptoms'] = breast_dataset.loc[i,'Symptoms'].split('&')
+    breast_dataset.at[i, 'Margin'] = breast_dataset.loc[i,'Margin'].split('&')
+    breast_dataset.at[i, 'Interpretation'] = breast_dataset.loc[i,'Interpretation'].split('&')
+    breast_dataset.at[i, 'Diagnosis'] = breast_dataset.loc[i,'Diagnosis'].split('&')
+
+    #read image
+    breast_dataset.at[i, 'Image_filename']  = cv2.imread(images_and_masks_foldername+breast_dataset.loc[i,'Image_filename'], cv2.IMREAD_UNCHANGED)
+    
+    #read tumor mask
+    if not isinstance(breast_dataset.loc[i,'Mask_tumor_filename'], float):
+        mask = cv2.imread(images_and_masks_foldername+breast_dataset.loc[i,'Mask_tumor_filename'], cv2.IMREAD_GRAYSCALE)>0
+        breast_dataset.at[i, 'Mask_tumor_filename']  = mask
+    else:
+        breast_dataset.at[i, 'Mask_tumor_filename'] = []
+
+    #read other mask
+    if not isinstance(breast_dataset.loc[i,'Mask_other_filename'], float):
+        masks_bool = []
+        for mask_path in breast_dataset.loc[i,'Mask_other_filename'].split('&'):
+            masks_bool.append(cv2.imread(images_and_masks_foldername+mask_path, cv2.IMREAD_GRAYSCALE)>0)
+        breast_dataset.at[i, 'Mask_other_filename'] = masks_bool
+    else:
+        breast_dataset.at[i, 'Mask_other_filename'] = []
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from torchvision.utils import save_image
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from PIL import Image
+
+# 定义生成器和判别器的网络结构
 class Generator(nn.Module):
-    def __init__(self, latent_dim, condition_dim, img_shape):
+    def __init__(self):
         super(Generator, self).__init__()
-
+        # 网络结构定义，可以根据需要进行修改
         self.model = nn.Sequential(
-            nn.Linear(latent_dim + condition_dim, 128),
+            nn.Linear(100 + num_classes, 128),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(128, 256),
             nn.BatchNorm1d(256, 0.8),
@@ -19,24 +67,26 @@ class Generator(nn.Module):
             nn.Linear(256, 512),
             nn.BatchNorm1d(512, 0.8),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, int(np.prod(img_shape))),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(1024, int(np.prod(img_shape))),
             nn.Tanh()
         )
 
-    def forward(self, z, c):
-        # z为随机噪声，c为条件信息
-        input = torch.cat((z, c), -1)
-        img = self.model(input)
+    def forward(self, z, labels):
+        # 生成器前向传播
+        gen_input = torch.cat((z, labels), -1)
+        img = self.model(gen_input)
         img = img.view(img.size(0), *img_shape)
         return img
 
-# 定义判别器模型
 class Discriminator(nn.Module):
-    def __init__(self, img_shape, condition_dim):
+    def __init__(self):
         super(Discriminator, self).__init__()
-
+        # 网络结构定义，可以根据需要进行修改
         self.model = nn.Sequential(
-            nn.Linear(np.prod(img_shape) + condition_dim, 512),
+            nn.Linear(img_shape[0] * img_shape[1] * img_shape[2] + num_classes, 512),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 256),
             nn.LeakyReLU(0.2, inplace=True),
@@ -44,103 +94,117 @@ class Discriminator(nn.Module):
             nn.Sigmoid()
         )
 
-    def forward(self, img, c):
-        input = torch.cat((img.view(img.size(0), -1), c), -1)
-        validity = self.model(input)
+    def forward(self, img, labels):
+        # 判别器前向传播
+        d_in = torch.cat((img.view(img.size(0), -1), labels), -1)
+        validity = self.model(d_in)
         return validity
 
 # 定义数据集类
-class CustomDataset(Dataset):
-    def __init__(self, data, labels, transform=None):
-        self.data = data
-        self.labels = labels
+# 定义数据集类
+class MedicalDataset(Dataset):
+    def __init__(self, dataframe, transform=None, target_size=(64, 64)):
+        self.dataframe = dataframe
         self.transform = transform
+        self.target_size = target_size
 
     def __len__(self):
-        return len(self.data)
+        return len(self.dataframe)
 
     def __getitem__(self, idx):
-        img = self.data[idx]
-        label = self.labels[idx]
+        # 从数据帧中获取图像和标签
+        img_path = str(self.dataframe.iloc[idx]['Image_filename'])
+        label = self.dataframe.iloc[idx]['Mask_tumor_filename']
 
+        # 读取图像
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+        # 将图像从BGR格式转换为RGB格式
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # 调整图像大小
+        img = cv2.resize(img, self.target_size)
+
+        # 如果需要的话，对图像进行转换
         if self.transform:
             img = self.transform(img)
 
         return img, label
 
-# 定义训练函数
-def train(generator, discriminator, dataloader, num_epochs, latent_dim, condition_dim, img_shape):
-    adversarial_loss = nn.BCELoss()
+# 定义数据转换
+transform = transforms.Compose([
+    transforms.ToTensor(), # 将图像转换为张量
+    # 如果还需要其他转换，可以继续添加
+])
 
-    optimizer_G = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    optimizer_D = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+# 设定随机种子以便复现
+torch.manual_seed(42)
 
-    for epoch in range(num_epochs):
-        for i, (imgs, labels) in enumerate(dataloader):
-
-            batch_size = imgs.shape[0]
-            valid = torch.ones(batch_size, 1).cuda()
-            fake = torch.zeros(batch_size, 1).cuda()
-
-            real_imgs = imgs.cuda()
-            labels = labels.cuda()
-
-            # 训练生成器
-            optimizer_G.zero_grad()
-
-            z = torch.randn(batch_size, latent_dim).cuda()
-            gen_imgs = generator(z, labels)
-            validity = discriminator(gen_imgs, labels)
-            g_loss = adversarial_loss(validity, valid)
-
-            g_loss.backward()
-            optimizer_G.step()
-
-            # 训练判别器
-            optimizer_D.zero_grad()
-
-            real_pred = discriminator(real_imgs, labels)
-            d_real_loss = adversarial_loss(real_pred, valid)
-
-            fake_pred = discriminator(gen_imgs.detach(), labels)
-            d_fake_loss = adversarial_loss(fake_pred, fake)
-
-            d_loss = (d_real_loss + d_fake_loss) / 2
-
-            d_loss.backward()
-            optimizer_D.step()
-
-            print(
-                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-                % (epoch, num_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-            )
-
-# 定义生成函数
-def generate(generator, num_samples, latent_dim, condition_dim, img_shape):
-    z = torch.randn(num_samples, latent_dim).cuda()
-    labels = torch.randint(0, 2, (num_samples, 1)).cuda()  # 假设有两种癌变类型
-    gen_imgs = generator(z, labels)
-    return gen_imgs
-
-# 参数设置
-latent_dim = 100
-condition_dim = 1
-img_shape = (3, 64, 64)  # 假设图像大小为64x64，3通道
-num_epochs = 200
+# 设定训练参数
+epochs = 100
 batch_size = 64
+lr = 0.0002
+b1 = 0.5
+b2 = 0.999
+latent_dim = 100
+num_classes = 10 # 假设有10个不同的诊断类别
+img_shape = (3, 64, 64) # 假设图像大小为64x64，并且是RGB图像
 
-# 加载数据集
-transform = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
-dataset = datasets.ImageFolder(root="data_path", transform=transform)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+# 加载数据
+medical_dataset = MedicalDataset(breast_dataset, transform=transform)
+dataloader = DataLoader(medical_dataset, batch_size=batch_size, shuffle=True)
 
-# 初始化模型、损失函数和优化器，并将它们移动到 GPU 上
-generator = Generator(latent_dim, condition_dim, img_shape).cuda()
-discriminator = Discriminator(img_shape, condition_dim).cuda()
+# 初始化生成器和判别器
+generator = Generator()
+discriminator = Discriminator()
+
+# 定义损失函数和优化器
+adversarial_loss = nn.BCELoss()
+optimizer_G = optim.Adam(generator.parameters(), lr=lr, betas=(b1, b2))
+optimizer_D = optim.Adam(discriminator.parameters(), lr=lr, betas=(b1, b2))
 
 # 训练模型
-train(generator, discriminator, dataloader, num_epochs, latent_dim, condition_dim, img_shape)
+for epoch in range(epochs):
+    for i, (imgs, labels) in enumerate(dataloader):
+        
+        # 训练判别器
+        optimizer_D.zero_grad()
+        
+        # 生成真实标签和假的标签
+        real_labels = torch.ones(imgs.size(0), 1)
+        fake_labels = torch.zeros(imgs.size(0), 1)
+        
+        # 生成噪声
+        z = torch.randn(imgs.size(0), latent_dim)
+        
+        # 生成假图像
+        gen_imgs = generator(z, labels)
+        
+        # 计算判别器的损失
+        real_loss = adversarial_loss(discriminator(imgs, labels), real_labels)
+        fake_loss = adversarial_loss(discriminator(gen_imgs.detach(), labels), fake_labels)
+        d_loss = (real_loss + fake_loss) / 2
+        
+        # 反向传播和优化
+        d_loss.backward()
+        optimizer_D.step()
+        
+        # 训练生成器
+        optimizer_G.zero_grad()
+        
+        # 生成假图像并计算生成器的损失
+        gen_imgs = generator(z, labels)
+        g_loss = adversarial_loss(discriminator(gen_imgs, labels), real_labels)
+        
+        # 反向传播和优化
+        g_loss.backward()
+        optimizer_G.step()
+        
+        # 打印训练信息
+        batches_done = epoch * len(dataloader) + i
+        if batches_done % 400 == 0:
+            print(f"[Epoch {epoch}/{epochs}] [Batch {i}/{len(dataloader)}] [D loss: {d_loss.item():.6f}] [G loss: {g_loss.item():.6f}]")
 
-# 生成新图像
-num_samples = 10
-generated_images = generate(generator, num_samples, latent_dim, condition_dim, img_shape)
+        # 保存生成的图像
+        if batches_done % 400 == 0:
+            save_image(gen_imgs.data[:25], f"images/{batches_done}.png", nrow=5, normalize=True)
---
+++ data_analysis.py
@@ -0,0 +1,44 @@
+
+#Required module
+#pip install openpyxl
+
+import pandas as pd
+import cv2
+
+#define .xlsx filename and folder containing images and masks
+info_filename = r'D:\MONAI\CGAN_Image_sup\CGAN_Image\data\BrEaST-Lesions-USG-clinical-data-Dec-15-2023.xlsx'
+images_and_masks_foldername = r'D:\MONAI\CGAN_Image_sup\CGAN_Image\data\BrEaST-Lesions_USG-images_and_masks-Dec-15-2023/'
+
+#read .xlsx file with clinical data
+breast_dataset = pd.read_excel(info_filename, sheet_name='BrEaST-Lesions-USG clinical dat')
+
+for i in breast_dataset.index:
+    #parse milti-label columns
+    breast_dataset.at[i, 'Tissue_composition'] = breast_dataset.loc[i,'Tissue_composition'].split('&')
+    breast_dataset.at[i, 'Signs'] = breast_dataset.loc[i,'Signs'].split('&')
+    breast_dataset.at[i, 'Symptoms'] = breast_dataset.loc[i,'Symptoms'].split('&')
+    breast_dataset.at[i, 'Margin'] = breast_dataset.loc[i,'Margin'].split('&')
+    breast_dataset.at[i, 'Interpretation'] = breast_dataset.loc[i,'Interpretation'].split('&')
+    breast_dataset.at[i, 'Diagnosis'] = breast_dataset.loc[i,'Diagnosis'].split('&')
+
+    #read image
+    breast_dataset.at[i, 'Image_filename']  = cv2.imread(images_and_masks_foldername+breast_dataset.loc[i,'Image_filename'], cv2.IMREAD_UNCHANGED)
+    
+    #read tumor mask
+    if not isinstance(breast_dataset.loc[i,'Mask_tumor_filename'], float):
+        mask = cv2.imread(images_and_masks_foldername+breast_dataset.loc[i,'Mask_tumor_filename'], cv2.IMREAD_GRAYSCALE)>0
+        breast_dataset.at[i, 'Mask_tumor_filename']  = mask
+    else:
+        breast_dataset.at[i, 'Mask_tumor_filename'] = []
+
+    #read other mask
+    if not isinstance(breast_dataset.loc[i,'Mask_other_filename'], float):
+        masks_bool = []
+        for mask_path in breast_dataset.loc[i,'Mask_other_filename'].split('&'):
+            masks_bool.append(cv2.imread(images_and_masks_foldername+mask_path, cv2.IMREAD_GRAYSCALE)>0)
+        breast_dataset.at[i, 'Mask_other_filename'] = masks_bool
+    else:
+        breast_dataset.at[i, 'Mask_other_filename'] = []
+
+#columns rename
+breast_dataset.rename(columns={"Image_filename": "Image", "Mask_tumor_filename": "Mask_tumor", "Mask_other_filename": "Mask_other"})
